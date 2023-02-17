2d569b96d22022c6c2c73ac7ed516300464f5c6d
Robert Teets <teetsrobert@gmail.com>
2023-02-17 16:18:22 -0700

    chore: slight styling tweaks and increased api revalidate time to 1 hour so cost savings.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,30 +25,30 @@ export default async function Home() {
                 aria-label="hero main header"
                 className='bg-[url("../../public/bg.jpg")] h-[100svh] text-slate-700 relative'
             >
-                <div className="blur-feathered w-1/2 flex flex-col items-start justify-start p-16 gap-64">
+                <nav className="fixed z-20 px-16 py-8">
                     <Link href="/">
                         <Image src={logo} alt="Granby Ranch" />
                     </Link>
-                    <div className="flex flex-col gap-3">
-                        <h1 className="text-4xl">
-                            PLAN YOUR NEXT
-                            <span className="text-6xl block">SKI VACATION</span>
-                        </h1>
-                        <p className="text-xl">
-                            Purchase your rentals and lessons in advance.
-                        </p>
-                        <div className="flex gap-3">
-                            <GoldButton href="/ski-snowboard-rentals">
-                                Rentals
-                            </GoldButton>
-                            <GoldButton href="/ski-snowboard-lessons">
-                                Lessons
-                            </GoldButton>
-                        </div>
+                </nav>
+                <div className="blur-feathered w-1/2 flex flex-col items-start justify-center p-16">
+                    <h1 className="text-4xl">
+                        PLAN YOUR NEXT
+                        <span className="text-6xl block">SKI VACATION</span>
+                    </h1>
+                    <p className="text-xl">
+                        Purchase your rentals and lessons in advance.
+                    </p>
+                    <div className="flex gap-3">
+                        <GoldButton href="/ski-snowboard-rentals">
+                            Rentals
+                        </GoldButton>
+                        <GoldButton href="/ski-snowboard-lessons">
+                            Lessons
+                        </GoldButton>
                     </div>
                 </div>
                 {/* footer options */}
-                <div className="bg-black absolute bottom-0 left-[32rem] right-[32rem] h-32 flex drop-shadow-2xl z-10">
+                <div className="bg-black absolute bottom-0 left-[32rem] right-[32rem] flex drop-shadow-2xl z-10">
                     <LinkCard classes="bg-blue-900" href="/tickets-passes">
                         <p>TICKETS & PASSES</p>
                     </LinkCard>
@@ -101,7 +101,7 @@ async function getWeatherData() {
     const headers = new Headers({ "x-api-key": process.env.API_KEY });
     const response = await fetch(
         "https://api.ambeedata.com/weather/latest/by-lat-lng?lat=40.043705&lng=-105.905750",
-        { headers, next: { revalidate: 600 } }
+        { headers, next: { revalidate: 3600 } }
     );
 
     if (!response.ok) {
--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -9,7 +9,7 @@ interface Props extends PropsWithChildren {
 export default function LinkCard({ href, classes, children, ...rest }: Props) {
     return (
         <Link
-            className={`${classes} flex-1 h-full font-thin text-white flex flex-col items-center justify-end hover:bg-slate-100 hover:text-black transition-colors pb-3 hover:shadow-inner`}
+            className={`${classes} flex-1 font-thin text-white flex flex-col items-center justify-center hover:bg-slate-100 hover:text-black transition-colors h-16 hover:shadow-inner`}
             href={href}
             {...rest}
         >
